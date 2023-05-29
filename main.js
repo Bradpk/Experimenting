fe3ed1d161b92b9475a055e2db01c1e18949f36d
Brad Kerr <brad.pk@live.com>
2023-05-29 02:46:44 +0000

    h

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-function doIt () {
+function doIt() {
     document.getElementById('change').style.background = "darkred"
 }
-
-    
+const btn = document.querySelector("button");
+btn.addEventListener("click", async () => {
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
+    const now = Tone.now();
+    synth.triggerAttackRelease("C4", "16n", now)
+    synth.triggerAttackRelease("Eb4", "16n", now + 0.3)
+    synth.triggerAttackRelease("G4", "16n", now + 0.6)
+    synth.triggerAttackRelease("G#4", "16n", now + 0.9)
+    synth.triggerAttackRelease("C5", "16n", now + 1.2)
+    synth.triggerAttackRelease("G#4", "16n", now + 2.1)
+    synth.triggerAttackRelease("G4", "16n", now + 2.4)
+    synth.triggerAttackRelease("G#4", "16n", now + 3.3)
+    synth.triggerAttackRelease("G4", "16n", now + 3.6)
+    synth.triggerAttackRelease("G#4", "16n", now + 3.7)
+    synth.triggerAttackRelease("G4", "16n", now + 3.8)
+    synth.triggerAttackRelease("F4", "16n", now + 3.9)
+    synth.triggerAttackRelease("G4", "16n", now + 4.2)
+    synth.triggerAttackRelease("B3", "16n", now + 4.5)
+    synth.triggerAttackRelease("C4", "16n", now + 4.8)
+});
